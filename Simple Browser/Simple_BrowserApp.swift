a0425c2913e955c4e5c6201c4ba4e2df4c1181ff
Elvich <grimaks333@bk.ru>
2025-04-04 03:01:04 +0300

    Initial Commit

---
+++ Simple Browser/Simple_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_BrowserApp.swift
+//  Simple Browser
+//
+//  Created by Maksim Gritsuk on 04.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Simple_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple BrowserTests/Simple_BrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_BrowserTests.swift
+//  Simple BrowserTests
+//
+//  Created by Maksim Gritsuk on 04.04.2025.
+//
+
+import Testing
+@testable import Simple_Browser
+
+struct Simple_BrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
