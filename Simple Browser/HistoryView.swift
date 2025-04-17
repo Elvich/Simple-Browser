0d24695cfde060765591d5cb21ffa7ffa8340dcb
Elvich <megahappy@bk.ru>
2025-04-17 23:52:08 +0300

    Добавил возможность вернуться с HistoryView в Web

--- Simple Browser/HistoryView.swift
+++ Simple Browser/HistoryView.swift
@@ -9,19 +9,32 @@ import SwiftUI
 import SwiftData
 
 struct HistoryView: View {
+    @Binding var state: ProgramState
+    
     @Query private var history: [Website]
     
     var body: some View {
         List{
             ForEach(history){ website in
-             
+                
                 Text(website.url)
                 
             }
         }
+        .gesture(
+            DragGesture(minimumDistance: 50, coordinateSpace: .local)
+                .onEnded { value in
+                    if value.translation.width > 0  {
+                        state = .searching
+                    }
+                })
     }
+    
 }
+    
+
+
 
 #Preview {
-    HistoryView()
+    ContentView()
 }
