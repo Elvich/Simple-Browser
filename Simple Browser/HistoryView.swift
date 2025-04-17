bb8ced5c8dd7bd3b911e75150dfc01b182ae756e
Elvich <megahappy@bk.ru>
2025-04-17 23:32:07 +0300

    Добавил аозможность смотреть историю поиска

---
+++ Simple Browser/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  Simple Browser
+//
+//  Created by Maksim Gritsuk on 17.04.2025.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
