5cf068fdf54bd8f388b6e5d05479354cf1b853ba
Elvich <megahappy@bk.ru>
2025-04-23 22:39:38 +0300

    Создал SearchingTests

---
+++ Simple BrowserTests/SearchingTests.swift
@@ -0,0 +1,16 @@
+//
+//  SearchingTests.swift
+//  Simple BrowserTests
+//
+//  Created by Maksim Gritsuk on 23.04.2025.
+//
+
+import Testing
+
+struct SearchingTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
